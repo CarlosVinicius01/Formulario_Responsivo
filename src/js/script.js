567cae0d400d1ee0b8afbe1e47e520f29e5a358b
c4rlosVinicius <pppcarlos8@gmail.com>
2025-01-28 20:59:23 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,195 @@
 const form = document.querySelector('#form')
 
+
+// campo obrigatório
 form.addEventListener('submit', function (e) {
     e.preventDefault()
 
+    const fields = [
+        {
+            id: 'name',
+            label: 'Nome',
+            validator: nameIsValid,
+        },
+        {
+            id: 'surname',
+            label: 'Sobrenome',
+            validator: nameIsValid,
+        },
+        {
+            id: 'birthdate',
+            label: 'Nascimento',
+            validator: dateIsValid,
+        },
+        {
+            id: 'email',
+            label: 'E-mail',
+            validator: emailIsValid,
+        },
+        {
+            id: 'password',
+            label: 'Senha',
+            validator: passwordIsSecure,
+        },
+        {
+            id: 'confirm_password',
+            label: 'Confirmar senha',
+            validator: passwordMatch,
+        },
+    ]
+
     const errorIcon = '<i class="fa-solid fa-circle-exclamation"></i>'
 
-    const name = document.querySelector('#name')
-    const inputBox = name.closest('.input-box')
-    const nameValue = name.value
+    fields.forEach(function (field) {
+        const input = document.getElementById(field.id)
+        const inputBox = input.closest('.input-box')
+        const inputValue = input.value
 
-    const errorSpan = inputBox.querySelector('.error')
-    errorSpan.innerHTML = ''
+        const errorSpan = inputBox.querySelector('.error')
+        errorSpan.innerHTML = ''
 
-    if (isEmpty(nameValue)) {
-        errorSpan.innerHTML = `${errorIcon} O campo é obrigatório`
-        return
-    }
+        const fieldValidator = field.validator(inputValue)
+        if (!fieldValidator.isValid) {
+            errorSpan.innerHTML = `${errorIcon} ${fieldValidator.errorMessage}`
+            return
+        }
+    })
+
+
 })
 
 function isEmpty(value) {
     return value === ''
 }
 
+// validar nome e sobrenome
+function nameIsValid(value) {
+    const validator = {
+        isValid: true,
+        errorMessage: null
+    }
+
+    if (isEmpty(value)) {
+        validator.isValid = false
+        validator.errorMessage = 'O campo é obrigatório!'
+        return validator
+    }
+
+    const min = 3
+
+    if (value.length < min) {
+        validator.isValid = false
+        validator.errorMessage = `O campo deve ter no mínimo ${min} caracteres!`
+        return validator
+    }
+
+    const regex = /^[a-zA-Z]/
+    if (!regex.test(value)) {
+        validator.isValid = false
+        validator.errorMessage = 'O campo deve conter apenas letras!'
+    }
 
 
+    return validator
+}
+
+// Validar data
+function dateIsValid(value) {
+    const validator = {
+        isValid: true,
+        errorMessage: null
+    }
+
+    if (isEmpty(value)) {
+        validator.isValid = false
+        validator.errorMessage = 'A data de nascimento é obrigatória!'
+        return validator
+    }
+
+    const year = new Date(value).getFullYear()
+
+    if (year < 1920 || year > new Date().getFullYear()) {
+        validator.isValid = false
+        validator.errorMessage = 'Data inválida!'
+        return validator
+    }
+
+    return validator
+}
+
+// Validar e-mail
+function emailIsValid(value) {
+    const validator = {
+        isValid: true,
+        errorMessage: null
+    }
+
+    if (isEmpty(value)) {
+        validator.isValid = false
+        validator.errorMessage = 'O e-mail é obrigatório!'
+        return validator
+    }
+    const regex = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+    if (!regex.test(value)) {
+        validator.isValid = false
+        validator.errorMessage = 'O e-mail precisa ser válido!'
+        return validator
+    }
+
+    return validator
+}
+
+// Validar senhas
+function passwordIsSecure(value) {
+    const validator = {
+        isValid: true,
+        errorMessage: null
+    }
+
+    if (isEmpty(value)) {
+        validator.isValid = false
+        validator.errorMessage = 'A senha é obrigatória!'
+        return validator
+    }
+
+    const regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+
+    if (!regex.test(value)) {
+        validator.isValid = false
+        validator.errorMessage = `
+            Sua senha deve conter ao menos: <br/>
+            8 dígitos <br/>
+            1 letra minúscula <br/>
+            1 letra maiúscula <br/>
+            1 número <br/>
+            1 caractere especial
+        `
+        return validator
+    }
+
+    return validator
+}
+
+function passwordMatch(value) {
+    const validator = {
+        isValid: true,
+        errorMessage: null
+    }
+
+    const passwordValue = document.getElementById('password').value
+
+    if (value === '' || passwordValue !== value) {
+        validator.isValid = false
+        validator.errorMessage = 'Senhas não condizem!'
+        return validator
+    }
+
+    return validator
+}
+
+
+
+// mostrar senha e olho
 const passwordIcons = document.querySelectorAll('.password-icon')
 
 passwordIcons.forEach(icon => {
